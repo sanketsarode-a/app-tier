577f950d70d517e1b9e41f5ad641146465132680
sanketsarode-a <sdsarode2812@gmail.com>
2024-07-01 22:15:47 +0530

    Add files via upload

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c7iaaie8ed57.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'DATABASE',
+    DB_DATABASE : ' webappdb'
+});
